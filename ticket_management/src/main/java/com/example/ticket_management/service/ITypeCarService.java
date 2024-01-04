4ef6ba9907e6c31e308896dd26595bad1809d951
viitran <tieuvi200904@gmail.com>
2024-01-04 15:31:00 +0700

    * fix entity

--- ticket_management/src/main/java/com/example/ticket_management/service/ITypeCarService.java
+++
@@ -1,7 +0,0 @@
-package com.example.ticket_management.service;
-
-import com.example.ticket_management.model.TypeCar;
-import com.example.ticket_management.service.common.IGenerationService;
-
-public interface ITypeCarService extends IGenerationService<TypeCar> {
-}
