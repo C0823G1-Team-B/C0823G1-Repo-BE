9e0dc07fafe4a831ea5cf6c47381d3c7f78f52e0
LamTamNhu <46390185+LamTamNhu@users.noreply.github.com>
2024-01-05 13:52:02 +0700

    +Gui email thanh toan, QR code.

---
+++ ticket_management/src/main/java/com/example/ticket_management/utils/BCryptUtils.java
@@ -0,0 +1,17 @@
+package com.example.ticket_management.utils;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+public class BCryptUtils {
+    public static String encryptPassword(String password) {
+        return BCrypt.hashpw(password, BCrypt.gensalt());
+    }
+    public static boolean checkPassword(String password, String hashedPassword) {
+        try {
+            return BCrypt.checkpw(password, hashedPassword);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return false;
+        }
+    }
+}
