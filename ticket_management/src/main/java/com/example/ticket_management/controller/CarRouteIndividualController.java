c02356ddf830e7dc03a4e0e5d72ac8eb8cbedd3e
toai2001 <phanmam001@gmail.com>
2024-01-08 09:24:39 +0700

    mamup

--- ticket_management/src/main/java/com/example/ticket_management/controller/CarRouteIndividualController.java
+++ ticket_management/src/main/java/com/example/ticket_management/controller/CarRouteIndividualController.java
@@ -1,12 +1,15 @@
 package com.example.ticket_management.controller;
 
 import com.example.ticket_management.model.CarRouteIndividual;
+import com.example.ticket_management.model.Ticket;
 import com.example.ticket_management.service.ICarRouteIndividualService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/car-route-individual")
@@ -17,6 +20,12 @@ public class CarRouteIndividualController {
     @GetMapping("")
     public ModelAndView showHome() {
         Iterable<CarRouteIndividual> carRouteIndividuals = iCarRouteIndividualService.findAll();
+        Optional<CarRouteIndividual> carRouteIndividual1 = iCarRouteIndividualService.findById(3);
+        Ticket ticket;
+        for (int i = 0; i < carRouteIndividual1.get().getCar().getTotalSeats(); i++) {
+            ticket = new Ticket();
+            ticket.setNumberSeat(i + 1);
+        }
         return new ModelAndView("car-route-individual", "criList", carRouteIndividuals);
     }
 }
