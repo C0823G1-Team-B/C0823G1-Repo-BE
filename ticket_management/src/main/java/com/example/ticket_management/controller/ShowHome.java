ee46bad09928d5b6d936ea6b102a74e375799f6c
giakiet2309 <135315001+giakiet2309@users.noreply.github.com>
2024-01-08 08:20:05 +0700

    check

--- ticket_management/src/main/java/com/example/ticket_management/controller/ShowHome.java
+++ ticket_management/src/main/java/com/example/ticket_management/controller/ShowHome.java
@@ -4,11 +4,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.time.LocalDateTime;
+import java.util.Date;
+
 @Controller
 public class ShowHome {
     @GetMapping("/")
     public String showHome(Model model) {
         model.addAttribute("a", "a");
+        LocalDateTime localDateTime = LocalDateTime.now();
+        Date date = new Date();
+        System.out.println(date);
+        System.out.println(localDateTime);
         return "home";
     }
 }
