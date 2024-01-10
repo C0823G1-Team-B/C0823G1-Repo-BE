b80add6f79def5bee9de5e0515db72e44278fb45
LamTamNhu <46390185+LamTamNhu@users.noreply.github.com>
2024-01-10 16:27:31 +0700

    *Mail confirm done, checkout result WIP

--- ticket_management/src/main/java/com/example/ticket_management/controller/EmailController.java
+++ ticket_management/src/main/java/com/example/ticket_management/controller/EmailController.java
@@ -7,14 +7,11 @@ import com.example.ticket_management.model.Ticket;
 import com.example.ticket_management.model.TicketCart;
 import com.example.ticket_management.service.ICustomerService;
 import com.example.ticket_management.service.IPaymentService;
-import com.example.ticket_management.utils.MailUtils;
+import com.example.ticket_management.utils.MailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,13 +24,13 @@ public class EmailController {
     @Autowired
     IPaymentService paymentService;
     @Autowired
-    MailUtils mailUtils;
+    MailService mailService;
 
 
     @PostMapping("/email_check")
-    public String checkMailExistInDatabase(CustomerDTO customerDTO,
+    public String checkMailExistInDatabase(@ModelAttribute("ctmDTO") CustomerDTO customerDTO,
                                            @SessionAttribute("ticketCart") TicketCart ticketCart,
-                                           Model model) {
+                                           RedirectAttributes redirectAttributes) {
         Customer customer = customerService.findByEmail(customerDTO.getEmail()).orElse(null);
         List<Ticket> ticketList = new ArrayList<>(ticketCart.ticketList.values());
         Payment currentPayment = paymentService.save(new Payment(ticketList));
@@ -41,17 +38,43 @@ public class EmailController {
         for (Ticket ticket : ticketList) {
             totalPrice += ticket.getPrice();
         }
-        if (customer == null) {
-            mailUtils.mailToConfirmCustomerEmail(totalPrice, currentPayment.getId(), customerDTO.getEmail());
 
-            model.addAttribute("message", "Quý khách vui lòng kiểm tra e-mail để tiếp tục thanh toán");
-            return "ticket";
-        } else {
-            return "redirect:/public/checkout?amount=" + totalPrice + "&payment_id=" + currentPayment.getId();
+        if (customer == null) {
+            Customer newCustomer = new Customer(customerDTO.getEmail(), customerDTO.getName(), customerDTO.getPhoneNumber(), ticketList);
+            newCustomer.setDelete(true);
+            customerService.save(newCustomer);
+            mailService.mailToConfirmCustomerEmail(totalPrice, currentPayment.getId(), customerDTO);
+
+            redirectAttributes.addFlashAttribute("ctmDTO", customerDTO);
+            redirectAttributes.addFlashAttribute("email_message", "Quý khách vui lòng kiểm tra e-mail để tiếp tục thanh toán");
+            return "redirect:/ticket/" + ticketList.get(0).getCarRouteIndividual().getId();
         }
+
+        if (customer.isDelete()) {
+            customer.setDelete(false);
+            customerService.save(customer);
+
+            mailService.mailToConfirmCustomerEmail(totalPrice, currentPayment.getId(), customerDTO);
+
+            redirectAttributes.addFlashAttribute("ctmDTO", customerDTO);
+            redirectAttributes.addFlashAttribute("email_message", "Quý khách vui lòng kiểm tra e-mail để tiếp tục thanh toán");
+            return "redirect:/ticket/" + ticketList.get(0).getCarRouteIndividual().getId();
+        }
+        return "redirect:/public/checkout?amount=" + totalPrice + "&payment_id=" + currentPayment.getId();
+
     }
+
     @GetMapping("/confirm_email")
-    public void confirmMail(){
-//        customerService.save(new Customer(customerDTO.getEmail(), customerDTO.getName(), customerDTO.getPhoneNumber(), ticketList));
+    public String confirmMail(@RequestParam("total_price") Long totalPrice,
+                              @RequestParam("payment_id") Integer paymentId,
+                              @RequestParam("email") String email) {
+        customerService.findByEmail(email).ifPresent(confirmedCustomer -> confirmedCustomer.setDelete(false));
+        Payment payment = paymentService.findById(paymentId).orElse(null);
+        if (payment != null) {
+            if (!payment.isStatus()) {
+                return "redirect:/public/checkout?amount=" + totalPrice + "&payment_id=" + paymentId;
+            }
+        }
+        return "redirect:/";
     }
 }
--- ticket_management/src/main/java/com/example/ticket_management/utils/MailService.java
+++ ticket_management/src/main/java/com/example/ticket_management/utils/MailService.java
@@ -1,5 +1,6 @@
 package com.example.ticket_management.utils;
 
+import com.example.ticket_management.dto.CustomerDTO;
 import jakarta.mail.Message;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.InternetAddress;
@@ -8,14 +9,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 @Component
-public class MailUtils {
+public class MailService {
     @Autowired
     JavaMailSender mailSender;
+    @Autowired
+    private TemplateEngine templateEngine;
 
     public void mailPaymentStatus(Integer paymentId) {
         String hashedPaymentId = BCryptUtils.encryptPassword(String.valueOf(paymentId));
@@ -26,8 +32,8 @@ public class MailUtils {
             message.setRecipients(Message.RecipientType.TO, InternetAddress.parse("pansuzu@gmail.com"));
             message.setSubject("Mã QR vé xe nhà xe XYZ");
             String mailBody = "<h2>Cảm ơn quý khách đã đặt vé xe. Vui lòng đưa mã QR dưới đây cho nhân viên xoát vé</h2>" +
-                    "QR Code <br>" +
-                    "<img src=\"https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=" + QRCodeCheck;
+                              "QR Code <br>" +
+                              "<img src=\"https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=" + QRCodeCheck;
             message.setContent(mailBody, "text/html");
         } catch (MessagingException e) {
             throw new RuntimeException(e);
@@ -43,20 +49,22 @@ public class MailUtils {
         return new ResponseEntity<>("Sai ma ticket", HttpStatus.NOT_ACCEPTABLE);
     }
 
-    public void mailToConfirmCustomerEmail(Long totalPrice, Integer paymentId, String customerEmail) {
+    public void mailToConfirmCustomerEmail(Long totalPrice, Integer paymentId, CustomerDTO customerDTO) {
         MimeMessage message = mailSender.createMimeMessage();
-        String confirmLink = "http://localhost:8080/public/checkout?amount=" + totalPrice + "&payment_id=" + paymentId;
-        System.out.println(confirmLink);
 
+        MimeMessageHelper helper = new MimeMessageHelper(message);
         try {
-            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(customerEmail));
-            message.setSubject("Xác nhận email thanh toán");
-            String mailBody = "<h2>Quý khách vui lòng click vào đường link dưới đây để tiếp tục thanh toán</h2>" +
-                    "<a src=\"" + confirmLink + "\">" + confirmLink + "</a>";
-            message.setContent(mailBody, "text/html");
+            helper.setTo(customerDTO.getEmail());
+            helper.setSubject("[Nhà xe Hiếu Hoa] Vui lòng xác nhận mail thanh toán");
+            Context context = new Context();
+            context.setVariable("username", customerDTO.getName());
+            context.setVariable("link", "http://localhost:8080/public/confirm_email?total_price=" +
+                                        totalPrice + "&payment_id=" + paymentId + "&email=" + customerDTO.getEmail());
+            String content = templateEngine.process("confirm-email", context);
+            helper.setText(content, true);
+            mailSender.send(message);
         } catch (MessagingException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
-        mailSender.send(message);
     }
 }
