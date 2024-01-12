68e48f4a39a34b34bc22672e6818a0b3dc6428b3
LamTamNhu <46390185+LamTamNhu@users.noreply.github.com>
2024-01-12 14:29:08 +0700

    *Google oauth2 WIP

--- ticket_management/src/main/java/com/example/ticket_management/config/WebSecurityConfig.java
+++ ticket_management/src/main/java/com/example/ticket_management/config/WebSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -68,14 +69,12 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers("/admin/**","/api/ticket/formCreateRoute","/api/ticket/formCreateRouteIndividual","/car-route-individual/**").hasRole("ADMIN")
                         .anyRequest().permitAll()
-
-
                 )
-
+                .oauth2Login(withDefaults())
                 .formLogin((formLogin) ->
                         formLogin.defaultSuccessUrl("/admin"))
                 .logout(logout ->
